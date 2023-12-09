7e0b8da97baa58d67c179dbc8a17006d7fa3bbf6
ShanRB <shanrb119@gmail.com>
2023-12-09 17:00:18 +1100

    Add Chapter 2

---
+++ csapp/cp2/p21.py
@@ -0,0 +1,17 @@
+# CSAPP Practice Problem 2.1
+
+# A
+hex_a = 0x39A7F8
+print(f"A. 0x{hex_a:0x} -> {bin(hex_a)}")
+
+# B
+bin_b = 0b1100100101111011
+print(f"B. {bin_b} -> {hex(bin_b)}")
+
+# C
+hex_c = 0xD5E4C
+print(f"C. 0x{hex_c:0x} -> {bin(hex_c)}")
+
+# D
+bin_d = 0b1001101110011110110101
+print(f"D. {bin_d} -> {hex(bin_d)}")
